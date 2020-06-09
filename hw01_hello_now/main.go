7eb04f441ce7c35e5298dbbf6746f4c20c89f0c9
Astrakhantsev Viktor <astrviktor@gmail.com>
2020-06-09 14:06:57 +0300

    [1] hw01 is completed

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
 func main() {
-	// Place your code here
+	timeFormat := "2 Jan 2006 15:04:05"
+
+	currentTime := time.Now()
+	currentTime = currentTime.Round(0)
+
+	exactTime, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+
+	if err == nil {
+		exactTime = exactTime.Round(0)
+
+		fmt.Println("current time:", currentTime.Format(timeFormat))
+		fmt.Println("exact time:", exactTime.Format(timeFormat))
+
+		os.Exit(0)
+	}
+
+	fmt.Println("error: ntp net error")
+	os.Exit(1)
 }

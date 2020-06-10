f3b52b90f9462c511408b60206da954be932aab0
Astrakhantsev Viktor <astrviktor@gmail.com>
2020-06-10 11:36:38 +0300

    [2] hw02 is completed (corrected linter msg)

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -5,12 +5,11 @@ import (
 	"strings"
 )
 
-// ErrInvalidString - ошибка распаковки
+// ErrInvalidString - ошибка распаковки.
 var ErrInvalidString = errors.New("invalid string")
 
-// Unpack - Распаковка строки
+// Unpack - Распаковка строки.
 func Unpack(str string) (string, error) {
-
 	// разделяем строку
 	splitStr := strings.Split(str, "")
 
@@ -21,7 +20,6 @@ func Unpack(str string) (string, error) {
 
 	// проверяем строку len = 1
 	if len(splitStr) == 1 {
-
 		// проверяем на число первый символ
 		if strings.Contains("0123456789", splitStr[0]) {
 			return "", ErrInvalidString
@@ -32,7 +30,6 @@ func Unpack(str string) (string, error) {
 
 	// строка len = 2 и больше
 	for i := 0; i < len(splitStr)-1; i++ {
-
 		// пустую строку пропускаем
 		if splitStr[i] == "" {
 			continue
@@ -45,18 +42,16 @@ func Unpack(str string) (string, error) {
 
 		// если следующее - число
 		if strings.Contains("0123456789", splitStr[i+1]) {
-
 			repeatCount := strings.Index("0123456789", splitStr[i+1])
 			repeatStr := strings.Repeat(splitStr[i], repeatCount)
 
 			splitStr[i] = repeatStr
 			splitStr[i+1] = ""
 		}
-
 	}
 
 	// соединяем строку
-	resStr := strings.Join(splitStr[:], "")
+	resStr := strings.Join(splitStr, "")
 
 	return resStr, nil
 }

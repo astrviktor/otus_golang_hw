a6d4a670c31fc78e808c430bb1fedb521a108658
Astrakhantsev Viktor <astrviktor@gmail.com>
2020-06-15 14:34:44 +0300

    [1] hw03 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,68 @@
 package hw03_frequency_analysis //nolint:golint,stylecheck
 
-func Top10(_ string) []string {
-	// Place your code here
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+// Top10 - топ 10 слов в строке.
+func Top10(text string) []string {
+	// символы под замену
+	var symbols = []string{".", "!", "?", ",", ";", " -", "- ",
+		" - ", "—", " —", "— ", " — ", "\n", "\t", `"`}
+
+	// переводим в нижний регистр
+	text = strings.ToLower(text)
+
+	// заменяем символы на пробелы
+	for _, symbol := range symbols {
+		text = strings.ReplaceAll(text, symbol, " ")
+	}
+
+	// разделяем на слова
+	strs := strings.Split(text, " ")
+
+	// перекладываем в map
+	frequencyStrings := make(map[string]int)
+
+	for _, s := range strs {
+		if s != "" {
+			frequencyStrings[s]++
+		}
+	}
+	// если map пустой то нет слов
+	if len(frequencyStrings) == 0 {
+		return nil
+	}
+
+	// перекладываем из map в slice структур которые можно будет сортировать
+	type StrCount struct {
+		Str   string
+		Count int
+	}
+
+	StrCountSlice := make([]StrCount, 0)
+
+	for key, value := range frequencyStrings {
+		StrCountSlice = append(StrCountSlice, StrCount{key, value})
+	}
+
+	// сортируем slice из слов и их количества
+	sort.Slice(StrCountSlice, func(i, j int) bool {
+		return StrCountSlice[i].Count > StrCountSlice[j].Count
+	})
+
+	// если количество элементов > 10 то отсекаем
+	if len(StrCountSlice) > 10 {
+		StrCountSlice = StrCountSlice[:10]
+	}
+
+	// нужно вернуть slice string
+	res := make([]string, 0)
+
+	for _, val := range StrCountSlice {
+		res = append(res, val.Str)
+	}
+
+	return res
 }
